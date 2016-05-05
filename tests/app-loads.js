76b549544259c74da9bb86cff4da33983d06c582
Loren Sands-Ramshaw <lorensr@gmail.com>
2016-05-05 15:17:50 -0400

    repro

---
+++ tests/app-loads.js
@@ -0,0 +1,9 @@
+describe('app loads', function () {
+  beforeEach(function () {
+    browser.url('http://localhost:3000');
+  });
+
+  it('loads @dev', function () {
+    assert.isOk(true);
+  });
+});
